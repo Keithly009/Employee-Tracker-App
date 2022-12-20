b489370cc2570a4857b556605feea19c552c4c18
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-12-20 04:57:40 -0800

    First commit, made the department.js file

---
+++ db/deparments.js
@@ -0,0 +1,13 @@
+const db = require("./db/connection");
+
+async function viewAllDepartments() { 
+    try {
+        const departments = 
+            await db.query('SELECT * FROM department').promise()
+        return departments     
+    } catch (err) {
+        console.log(err)
+    } 
+}
+
+module.exports = { viewAllDepartments }
--- index.js
+++ index.js
@@ -2,8 +2,29 @@
 const { prompt } = require("inquirer");
 const db = require("./db/connection");
 
-const start = () => {
+// Use the async function to skip .then functions 
+const start = async () => {
     console.log("Welcome to the Employee Manager!");
+    prompt([
+        {
+            type: "list", 
+            name: "Choice",
+            message: "What would you like to do?",  
+            choices: [
+                'View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role', 
+                'Exit'
+            ]
+        }
+    ])
+    switch (choice) { 
+        case 'View all departments': 
+    }
 }
 
 start();
